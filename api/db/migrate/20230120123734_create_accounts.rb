be9bcb83dd92c9d1179f65c0d3f4e495d0b5ea79
rich <adukyerer@gmail.com>
2023-01-20 12:48:16 +0000

    Create migration and associations

--- api/db/migrate/20230120123734_create_accounts.rb
+++ api/db/migrate/20230120123734_create_accounts.rb
@@ -1,6 +1,9 @@
 class CreateAccounts < ActiveRecord::Migration[7.0]
   def change
     create_table :accounts do |t|
+      t.string :account_number
+      t.decimal :balance
+      t.references :client, null: false, foreign_key: true
 
       t.timestamps
     end
--- api/db/migrate/20230120123806_create_transactions.rb
+++ api/db/migrate/20230120123806_create_transactions.rb
@@ -1,6 +1,10 @@
 class CreateTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :transactions do |t|
+      t.string :transaction_number
+      t.string :transaction_type
+      t.decimal :amount
+      t.references :account, null: false, foreign_key: true
 
       t.timestamps
     end
